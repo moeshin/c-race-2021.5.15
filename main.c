afd3c82ed6c3a6a1c1940ca34ad7de26c97a7813
MoeShin <hi@moeshin.com>
2021-05-15 09:58:51 +0800

    save

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
-#include "prime-number.h" // 素数
+//#include "prime-number.h" // 素数
 //#include "factorial.h" // 阶乘
 //#include "narcissistic-number.h" // 水仙花数
 //#include "fibonacci-sequence.h" // 斐波那契数列
 //#include "array.h" // 数组
-//#include "struct.h" // 结构体，还没写好
+#include "struct.h" // 结构体
 
 int main() {
     run();
--- struct.h
+++ struct.h
@@ -10,41 +10,75 @@ Item {
 };
 
 Array {
-    int length;
     Item *next;
 };
 
 Array *array_init() {
     Array *arr = malloc(sizeof(Array));
-    arr->length = 0;
     arr->next = NULL;
     return arr;
 }
 
-void array_append(Array *arr, int data) {
-    Item next;
-    if (arr->length == 0) {
-        arr->next = &next;
+int array_length(Array *arr) {
+    int len = 0;
+    Item *next = arr->next;
+    if (next != NULL) {
+        ++len;
+        while ((next = next->next) != NULL) {
+            ++len;
+        }
     }
-    return &next;
+    return len;
+}
+
+void array_append(Array *arr, int data) {
+    Item *next = malloc(sizeof(Item));
+    next->next = NULL;
+    next->data = data;
+    Item *n = arr->next;
+    if (n == NULL) {
+        arr->next = next;
+    } else {
+        while (n->next != NULL) {
+            n = n->next;
+        }
+        printf("n->data = %d\n", n->data);
+        n->next = next;
+    }
+}
+
+void item_free(Item *next) {
+    if (next != NULL) {
+        item_free(next->next);
+        free(next);
+    }
 }
 
 void array_free(Array *arr) {
-
+    item_free(arr->next);
+    free(arr);
 }
 
 void run() {
     Array *arr = array_init();
-    struct s_item *current = NULL;
     char str[8];
+    printf("Please input data, input . to exit:\n");
     while (1) {
         scanf("%s", str);
         if (!strcmp(str, ".")) {
             break;
         }
-        struct s_item next;
-        next = ;
+        array_append(arr, strtol(str, NULL, 10));
     }
-    printf("%s", str);
-//    array
+
+    printf("Array data:\n");
+    Item *next = arr->next;
+    if (next != NULL) {
+        printf("%d\n", next->data);
+        while ((next = next->next) != NULL) {
+            printf("%d\n", next->data);
+        }
+    }
+    printf("length: %d", array_length(arr));
+    array_free(arr);
 }
